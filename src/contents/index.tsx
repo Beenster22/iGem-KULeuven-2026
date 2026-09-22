30dbc6b04eb9ff83b215403ce9dadd640a2d34cd
Brandon Joubert <brandon.joubert22@gmail.com>
2026-09-22 20:23:03 +0200

    page structure update

--- src/contents/index.tsx
+++ src/contents/index.tsx
@@ -9,18 +9,24 @@ export { default as Engineering } from "./engineering.mdx";
 export { default as Contribution } from "./contribution.mdx";
 export { default as SafetyAndSecurity } from "./safety-and-security.mdx";
 // Dry Lab
-export { default as Crispr } from "./crispr.mdx";
-export { default as Biocontainment } from "./biocontainment.mdx";
 export { default as PromotersAndRbs } from "./promoters-and-rbs.mdx";
+export { default as CodonOptimizationTool } from "./codon-optimization-tool.mdx";
+export { default as BshScreening } from "./bsh-screening.mdx";
 // Wet Lab
 export { default as Experiments } from "./experiments.mdx";
 export { default as Protocols } from "./protocols.mdx";
 export { default as Notebook } from "./notebook.mdx";
 export { default as Results } from "./results.mdx";
-export { default as AlternativePlatform } from "./alternative-platform.mdx";
-export { default as ProofOfConcept } from "./proof-of-concept.mdx";
 // iHP
 export { default as HumanPractices } from "./human-practices.mdx";
 export { default as Events } from "./events.mdx";
 // Entrepreneurship
 export { default as Entrepreneurship } from "./entrepreneurship.mdx";
+// Prizes and Medals / Judging Form
+export { default as GoldMedal } from "./gold-medal.mdx";
+export { default as BestWiki } from "./best-wiki.mdx";
+export { default as BestPresentation } from "./best-presentation.mdx";
+export { default as AlternativePlatform } from "./alternative-platform.mdx";
+export { default as BestPartCollection } from "./best-part-collection.mdx";
+export { default as BestEntrepreneurship } from "./best-entrepreneurship.mdx";
+export { default as BestIhp } from "./best-ihp.mdx";
